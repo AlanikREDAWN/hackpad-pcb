9a8f03cf69505a83cd7a79fca8e072874d334ea5
Alanik <lilia.dostie@socprep.org>
2024-09-24 17:32:44 -0400

    hackpad progress

--- Firmware/main.py
+++ Firmware/main.py
@@ -6,6 +6,39 @@ from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.extensions.display import Display, TextEntry, ImageEntry
 from kmk.extensions.display.ssd1306 import SSD1306
+from kmk.modules.layers import Layers
+from kmk.modules.macros import Macros
+from kmk.modules.macros import Press, Release, Tap, Delay
+
+macros = Macros()
+
+
+# class LayerTracker(RGB):
+#     def on_layer_change(self, layer):
+#         if layer == 0:
+#             display.entries = [
+#                 ImageEntry(image="catswhite.bmp", x=0, y=0),
+#                 TextEntry(text="Layer 0", x=64, y=32, x_anchor="B", y_anchor="M")
+#             ]
+#             keyboard.extensions.append(display)
+#         elif layer == 1:
+#             display.entries = [
+#                 ImageEntry(image="catswhite.bmp", x=0, y=0),
+#                 TextEntry(text="Layer 1", x=64, y=32, x_anchor="B", y_anchor="M")
+#             ]
+#             keyboard.extensions.append(display)
+#         elif layer == 2:
+#             display.entries = [
+#                 ImageEntry(image="catswhite.bmp", x=0, y=0),
+#                 TextEntry(text="Layer 2", x=64, y=32, x_anchor="B", y_anchor="M")
+#             ]
+#             keyboard.extensions.append(display)
+#         elif layer == 4:
+#             display.entries = [
+#                 ImageEntry(image="catswhite.bmp", x=0, y=0),
+#                 TextEntry(text="Layer 1", x=64, y=32, x_anchor="B", y_anchor="M")
+#             ]
+#             keyboard.extensions.append(display)            # white
 
 COL0 = board.D10
 COL1 = board.D9
@@ -20,6 +53,49 @@ ROW3 = board.D0
 
 print("Starting")
 keyboard = KMKKeyboard()
+layers = Layers()
+
+col_pins = (COL0, COL1, COL2, COL3, COL4)
+row_pins = (ROW0, ROW1, ROW2, ROW3)
+diode_orientation = DiodeOrientation.COL2ROW
+
+keyboard.modules.append(layers)
+keyboard.modules.append(macros)
+
+# Layers
+TOGGLE = KC.TG(1)
+
+CONTROL_S_MACRO = KC.MACRO(
+    Press(KC.RCTRL),
+    Press(KC.S),
+    Delay(500),
+    Release(KC.RCTRL),
+    Release(KC.S),
+)
+
+ALT_S = KC.RALT(KC.S)
+
+ALT_T = KC.RALT(KC.T)
+
+ALT_F = KC.RALT(KC.F)
+
+ALT_V = KC.RCALT(KC.V)
+
+ALT_M = KC.RCALT(KC.M)
+
+
+
+keyboard.keymap = [
+    # Base Layer
+    [
+
+    ],
+
+    # Function Layer
+    [
+
+    ],
+]
 
 i2c_bus = busio.I2C(board.GP_SCL, board.GP_SDA)
 
@@ -30,6 +106,9 @@ driver = SSD1306(
 
 display = Display(
     display=driver,
+    entries=[
+        ImageEntry(image="catswhite.bmp", x=0, y=0),
+    ]
     width=128, # screen size
     height=32, # screen size
     flip = False, # flips your display content
@@ -45,9 +124,7 @@ display = Display(
     powersave_off_time=30, # time in seconds to turn off screen
 )
 
-col_pins = (COL0, COL1, COL2, COL3, COL4)
-row_pins = (ROW0, ROW1, ROW2, ROW3)
-diode_orientation = DiodeOrientation.COL2ROW
+keyboard.extensions.append(display)
 
 if __name__ == '__main__':
     keyboard.go()
